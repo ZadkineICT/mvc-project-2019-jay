61ba6b17b696ed9b84ccc1def80f9ca6a9830d5c
Akash <akashramkisoen@outlook.com>
2019-05-25 19:18:44 +0200

    Rooms

--- ProjectJay/database/migrations/2019_05_23_085431_create_rooms_table.php
+++ ProjectJay/database/migrations/2019_05_23_085431_create_rooms_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateRoomsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('rooms', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name', 100);
-            $table->string('email', 100)->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password', 255);
-            $table->rememberToken();
+            $table->string('room_size', 50);
+            $table->string('hotel_id');
             $table->timestamps();
         });
     }
@@ -31,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('rooms');
     }
 }
--- ProjectJay/database/seeds/DatabaseSeeder.php
+++ ProjectJay/database/seeds/DatabaseSeeder.php
@@ -13,5 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         $this->call(HotelsTableSeeder::class);
+
+        $this->call(RoomsTableSeeder::class);
     }
 }
