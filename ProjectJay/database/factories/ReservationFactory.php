54e9e6bce6ce9379a9fa6b7c2a2fadb9d3e5df0c
Akash <akashramkisoen@outlook.com>
2019-05-27 10:19:52 +0200

    Reservation

---
+++ ProjectJay/database/factories/ReservationFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Reservation;
+use Faker\Generator as Faker;
+
+$factory->define(Reservation::class, function (Faker $faker) {
+    return [
+        //
+        'date_start' => $faker->date(),
+        'date_end' => $faker->date(),
+        'price' => $faker->randomFloat(2,1,99),
+        'number_of_persons' => $faker->numberBetween(1,6),
+        'user_id' => \App\User::all()->random()->id,
+        'room_id' => \App\Room::all()->random()->id,
+        'hotel_id' => \App\Hotel::all()->random()->id,
+    ];
+});
