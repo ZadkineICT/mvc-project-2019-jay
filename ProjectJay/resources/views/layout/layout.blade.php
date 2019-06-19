bd08516d243c88f4a148cec4f7e49c2e5625566c
JasperSjouw <jwsjouw@gmaillcom>
2019-06-19 16:46:22 +0200

    Nav Rework, White, Dropdown

--- ProjectJay/resources/views/layout/layout.blade.php
+++ ProjectJay/resources/views/layout/layout.blade.php
@@ -4,6 +4,9 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
+    integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay"
+    crossorigin="anonymous">
     <link rel="icon" href="{{ asset('favicon.ico') }}">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('scss/main.scss') }}" rel="stylesheet">
--- ProjectJay/resources/views/layout/nav.blade.php
+++ ProjectJay/resources/views/layout/nav.blade.php
@@ -1,49 +1,41 @@
-{{-- THIS IS A TEST FILE --}}
-
 @section('nav')
-<li><a class="nav-left" href="/frontpage">Home</a></li>
-<li><a class="nav-left" href="{{ route('hotels.show', '') }}">Hotels</a></li>
-@role('owner|admin')
-<li><a class="nav-left" href="{{ route('rooms.show', '') }}">Rooms</a></li>
-<li><a class="nav-left" href="{{ route('reservations.show', '') }}">Reservations</a></li>
-<li><a class="nav-left" href="{{ route('employees.show', '') }}">Employees</a></li>
-<li><a class="nav-left" href="{{ route('roomtypes.show', '') }}">Roomtypes</a></li>
-<li><a class="nav-left" href="{{ route('reviews.show', '') }}">Reviews</a></li>
-@endrole
-@hasanyrole('admin|client')
-<li><a class="nav-left" href="/home">User-home</a></li>
-@endrole
-
-<li><form action=""><input class="nav-mid" type="text" placeholder="Search"></form></li>
-
-<!-- Authentication Links -->
+<input class="nav-right menu-btn" type="checkbox" id="menu-btn"/>
+<label class="nav-right menu-icon" for="menu-btn"><span class="navicon"></span></label>
 @guest
-    @if (Route::has('register'))
-        <li class="nav-item">
-            <a class="nav-link nav-right" href="{{ route('register') }}">{{ __('Register') }}</a>
-        </li>
+        @if (Route::has('register'))
+            <li><a class="nav-right" href="{{ route('register') }}">{{ __('Register') }}</a></li>
+        @endif
+        <li><a class="nav-right" href="{{ route('login') }}">{{ __('Login') }}</a></li>
     @else
-        <div class="nav-right nav-item dropdown">
-            <ul>
-                <li>
-                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                        {{ Auth::user()->name }} <span class="caret"></span>
-                    </a>
-                </li>
-                <li>
-                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="{{ route('logout') }}"
-                        onclick="event.preventDefault();
-                                                        document.getElementById('logout-form').submit();">
-                            {{ __('Logout') }}test
-                        </a>
+    @endguest
+@hasanyrole('owner|admin|client')
+    <li><a class="nav-right" href="/home">{{ Auth::user()->name }}</a></li>
+    <li><form action=""><input class="nav-mid" type="text" placeholder="Search"></form></li>
+@endrole
+<ul class="navmenu">
+    <li><a class="nav-left" href="/frontpage">Home</a></li>
+    <li><a class="nav-left" href="{{ route('hotels.show', '') }}">Hotels</a></li>
+    @role('owner|admin')
+    <ul class="adminmenu">
+        <li><a class="nav-left" href="{{ route('rooms.show', '') }}">Rooms</a></li>
+        <li><a class="nav-left" href="{{ route('reservations.show', '') }}">Reservations</a></li>
+        <li><a class="nav-left" href="{{ route('employees.show', '') }}">Employees</a></li>
+        <li><a class="nav-left" href="{{ route('roomtypes.show', '') }}">Roomtypes</a></li>
+        <li><a class="nav-left" href="{{ route('reviews.show', '') }}">Reviews</a></li>
+    </ul>
+    @endrole
+    @guest
+        @if (Route::has('register'))
+        @endif
+    @else
+        <li>
+            <a class="" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+            <i class="fas fa-sort-up"></i> {{ __('Logout') }}
+            </a>
 
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                            @csrf
-                        </form>
-                    </div>
-                </li>
-            </ul>   
-        </div>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
+        </li>
     @endguest
 @endsection 
